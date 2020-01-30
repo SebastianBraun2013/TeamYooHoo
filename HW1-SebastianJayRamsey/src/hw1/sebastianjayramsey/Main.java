694d98f868bce704077fa64bf7a31e80d031c74e
jay.hund <jay.hund@greedo>
2020-01-29 20:53:18 -0600

    saved ur life

--- HW1-SebastianJayRamsey/src/hw1/sebastianjayramsey/Main.java
+++ HW1-SebastianJayRamsey/src/hw1/sebastianjayramsey/Main.java
@@ -5,18 +5,19 @@
  */
 package hw1.sebastianjayramsey;
 
+import java.util.Scanner;
+
 /**
  *
  * @author sebastian.braun
  */
 public class Main {
 
-     /* https://stackoverflow.com/questions/140131/
-     * convert-a-string-representation-of-a-hex-dump-to-a-byte-array-using-java
-     *
-     * @param s
-     * @return
+    /**
+     * @param args the command line arguments
      */
+    //*https://stackoverflow.com/questions/140131/
+    //convert-a-string-representation-of-a-hex-dump-to-a-byte-array-using-java
     public static byte[] hexStringToByteArray(String s) {
         int len = s.length();
         byte[] data = new byte[len / 2];
@@ -26,16 +27,29 @@ public class Main {
         }
         return data;
     }
+    
+    //String to Byte Array
+    
 
-    public static boolean TEACheck(byte[] key){
-        return key.length % 16 == 0;
+    public static boolean TEAKeyCheck(byte[] key){
+        return key.length == 16;
     }
     
-    //base64 encoding of encrypted byte array
-    //plaintext message -> byte[]
-    
-    public static void main(String[] args) {
+    //Base64 encoding
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = "4d68023308dacfac5ee8d54a14d00caa";
+        byte[] key = hexStringToByteArray(str);
+        for (int i = 0; i < key.length; i++) {
+            //https://stackoverflow.com/questions/12310017/
+            //how-to-convert-a-byte-to-its-binary-string-representation 
+            String s1 = String.format("%8s", Integer.toBinaryString(key[i] & 0xFF)).replace(' ', '0');
+            System.out.print(s1 +" ");
+        }
+                    System.out.println("");
+                    System.out.println(key.length);
+        System.out.println(TEAKeyCheck(key) +"");
     }
 
 }
