741a739acc10a69d0240357bdc7a20ad58a6120b
RamseyKerley <kerleyramsey@gmail.com>
2020-01-29 19:37:14 -0600

    bannananana

--- HW1-SebastianJayRamsey/src/hw1/sebastianjayramsey/RC4.java
+++ HW1-SebastianJayRamsey/src/hw1/sebastianjayramsey/RC4.java
@@ -6,13 +6,14 @@
 package hw1.sebastianjayramsey;
 
 /**
- *
+ *Me and Sebastian found code for rc4 implementation from Stack overflow
+ * https://stackoverflow.com/questions/12289717/rc4-encryption-java
  * @author sebastian.braun
  */
 public class RC4 {
     
-    private byte[] Message;
-    private byte[] Key;
+    private final byte[] Message;
+    private final byte[] Key;
 
     public void RC4(byte[] k, byte[] m) {
         Message = m;
