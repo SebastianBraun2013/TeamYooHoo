7ecb98838746e0ad6d4c4e6ca47b0ed6a9530abe
sebastian.braun <sebastian.braun@HU130C05.Blackburn.College>
2020-01-29 18:22:03 -0600

    Redo

---
+++ HW1-SebastianJayRamsey/src/hw1/sebastianjayramsey/RC4.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw1.sebastianjayramsey;
+
+/**
+ *
+ * @author sebastian.braun
+ */
+public class RC4 {
+    
+    private byte[] Message;
+    private byte[] Key;
+
+    public void RC4(byte[] k, byte[] m) {
+        Message = m;
+        Key = k;
+
+    }
+
+    public byte[] rc4Encryption() {
+        byte[] cyphertext = null;
+
+        byte[] message = this.Message;
+
+        return cyphertext;
+    }
+
+    public byte[] rc4Decryption(byte[] cyphertext) {
+        byte[] cText = cyphertext;
+
+        byte[] message = null;
+
+        return message;
+    }
+}
